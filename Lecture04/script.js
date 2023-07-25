42fc71c501cbb4735359568093e084086bae4aa7
Tapan Kumar Mandal <tapankumar29808.com>
2023-07-25 08:46:25 +0530

    my html css js code lect 1,2,3 and 4

---
+++ Lecture04/script.js
@@ -0,0 +1,25 @@
+let x =55;
+console.log(x)
+let fname = "Shyam"
+console.log(fname)
+let p="45"
+let q="55"
+console.log(q-20)
+q=Number(q)
+console.log(typeof(q))
+
+let lname="Yadav"
+
+let s = "I"+" "+"am "+fname+" "+lname
+console.log(s)
+
+console.log(`my name is ${fname} ${lname}`)
+
+let n =15
+if(n<18){
+    console.log("you are not eligible")
+}
+else
+{
+    console.log("you are eligible")
+}
